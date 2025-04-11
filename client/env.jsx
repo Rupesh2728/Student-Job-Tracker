ec4a49bf7c3d98c7842178d7e80951aa8c41f97c
Rupesh_peddineni <rupeshprofessional2728@gmail.com>
2025-04-11 17:55:58 +0530

    small changes in frontend code

--- client/env.jsx
+++ client/env.jsx
@@ -1,3 +1,3 @@
 // export const REACT_APP_BACKEND_URL = "http://localhost:5000"
 
-export const REACT_APP_BACKEND_URL = "https://student-job-tracker-mexm.onrender.com"
+export const REACT_APP_BACKEND_URL = "https://student-job-tracker-c9qi.onrender.com"
